0dd821c5a52bbaf221b47789271a23adf7a16f55
mariushu <36562086+mariushu@users.noreply.github.com>
2018-03-03 10:43:48 +0100

    Update Point.java

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -11,7 +11,7 @@ public class Point {
 
   public  double distance(Point p2) {
 
-    return Math.sqrt((p2.x - this.x) + (p2.y - this.y));
+    return Math.sqrt((p2.x - this.x)*(p2.x - this.x) + (p2.y - this.y)*(p2.y - this.y));
   }
 
 
