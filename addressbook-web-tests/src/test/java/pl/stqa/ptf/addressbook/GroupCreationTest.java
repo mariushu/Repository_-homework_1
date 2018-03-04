356acc44599e9f918e0a602abccf4d0f231b9673
mariushu <36562086+mariushu@users.noreply.github.com>
2018-03-04 09:22:21 +0100

    Corrected excercise 4.1 with chrome driver

--- addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/GroupCreationTest.java
@@ -2,8 +2,8 @@ package pl.stqa.ptf.addressbook;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,17 +11,19 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class GroupCreationTest {
-  FirefoxDriver wd;
+  ChromeDriver wd;
 
   @BeforeMethod
   public void setUp() throws Exception {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
   }
 
   @Test
   public void testGroupCreation() {
     wd.get("http://localhost/addressbook/group.php");
+
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
