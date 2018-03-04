9f119fdccb568e1bb97413c413cb90603094dc0f
mariushu <36562086+mariushu@users.noreply.github.com>
2018-03-04 07:36:37 +0100

    Update Distanceprogram.java

--- sandbox/src/main/java/Distanceprogram.java
+++ sandbox/src/main/java/Distanceprogram.java
@@ -1,15 +1,11 @@
 public class Distanceprogram {
 
     public static void main(String[] args) {
-
         Point p1 = new Point  (2,5);
-
-        Point p2 = new Point  (5, 9);
-
-
+        Point p2 = new Point  (5,9);
+        
         System.out.println("Distance between two points is " + p1.distance(p2));
 
     }
 }
-
 
