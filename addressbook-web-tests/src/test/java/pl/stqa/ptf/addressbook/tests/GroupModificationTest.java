88f2569937ba3c3384b18b8d8713353a7dc11990
mariushu <36562086+mariushu@users.noreply.github.com>
2018-03-20 21:49:10 +0100

    Lesson 4.5 Collection creating

--- addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/GroupModificationTest.java
@@ -4,24 +4,28 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.stqa.ptf.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTest extends TestBase{
 
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().goToGroupPage();
 
-    int before = app.getGroupHelper().getGroupCount();
+
     if  (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    //int before = app.getGroupHelper().getGroupCount();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test", "test", "test"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnGroupPage();
-
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    //int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after.size(), before.size());
   }
 
 
