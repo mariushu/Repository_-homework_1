c7564afab68a37b92d60a25fd0b8bc1c1618c9f7
mariushu <36562086+mariushu@users.noreply.github.com>
2018-03-21 22:32:33 +0100

    Lesson 4.8 Find the maximum value

--- addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/model/GroupData.java
@@ -3,28 +3,35 @@ package pl.stqa.ptf.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private final String id;
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public String getId() {
+  public int getId() {
     return id;
   }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+
   public String getName() {
+
     return name;
   }
 
@@ -43,16 +50,13 @@ public class GroupData {
             ", name='" + name + '\'' +
             '}';
   }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
-
     GroupData groupData = (GroupData) o;
-
-    if (id !=null ? !id.equals(groupData.id) : groupData.id != null) return false;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
+    return id == groupData.id &&
+            Objects.equals(name, groupData.name);
   }
 
   @Override
--- addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/GroupCreationTest.java
@@ -4,20 +4,33 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.stqa.ptf.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
-public class GroupCreationTest  extends TestBase{
+public class GroupCreationTest  extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-
-    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    GroupData group = new GroupData("test1", null, null);
+    app.getGroupHelper().createGroup(group);
 
     List<GroupData> after = app.getGroupHelper().getGroupList();
 
-    Assert.assertEquals(after.size (), before.size() + 1 );
-  }
+    Assert.assertEquals(after.size(), before.size() + 1);
 
+    int max = 0;
+    {
+      for (GroupData g : after) {
+        if (g.getId() > max) {
+
+        }
+      }
+      group.setId(max);
+      before.add(group);
+      Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    }
+
+  }
 }
