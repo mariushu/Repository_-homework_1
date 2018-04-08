2e3c4d4f22fb6a6c9dd73ca1f3fbfb7cec1b3764
mariushu <36562086+mariushu@users.noreply.github.com>
2018-04-08 17:52:56 +0200

    5.5 Unique Identifiers

--- addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/appmenager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/appmenager/GroupHelper.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import pl.stqa.ptf.addressbook.model.GroupData;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -36,8 +37,9 @@ public class GroupHelper extends HelperBase {
     click(By.name("delete"));
   }
 
-  public void selectGroup(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
+
+  public void selectGroupById(int id) {
+    wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
   }
 
 
@@ -55,17 +57,20 @@ public class GroupHelper extends HelperBase {
     submitGroupCreation();
     returnGroupPage();
   }
-  public void modify(int index, GroupData group) {
-    selectGroup(index);
+  public void modify( GroupData group) {
+    selectGroupById(group.getId());
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
     returnGroupPage();
   }
-  public void delete(int index) {
-    selectGroup(index);
+
+
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroup();
     returnGroupPage();
+
   }
 
   public boolean isThereAGroup() {
@@ -77,8 +82,10 @@ public class GroupHelper extends HelperBase {
 
   }
 
-  public List<GroupData> list() {
-    List<GroupData> groups = new ArrayList<GroupData>();
+  
+
+  public Set<GroupData> all () {
+    Set<GroupData> groups = new HashSet<GroupData>();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
@@ -87,4 +94,6 @@ public class GroupHelper extends HelperBase {
     }
     return groups;
   }
+
+
 }
--- addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/GroupCreationTest.java
@@ -4,33 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.stqa.ptf.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupCreationTest  extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.goTo().GroupPage();
-    List<GroupData> before = app.group().list();
+    Set<GroupData> before = app.group().all();
     GroupData group = new GroupData().withName("test2");
     app.group().create(group);
 
-    List<GroupData> after = app.group().list();
+    Set<GroupData> after = app.group().all();
 
     Assert.assertEquals(after.size(), before.size() + 1);
 
-
-
-
-      //group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-      before.add(group);
-      Comparator<? super GroupData> byID = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-      before.sort(byID);
-      after.sort(byID);
-
-
-      Assert.assertEquals(before, after);
+    group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt());
+    before.add(group);
+    Assert.assertEquals(before, after);
     }
 
   }
