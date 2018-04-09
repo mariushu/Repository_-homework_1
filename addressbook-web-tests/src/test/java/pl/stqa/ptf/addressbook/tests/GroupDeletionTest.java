fd6dddf217c45acdc9c5871ac73df8132db0b0a1
mariushu <36562086+mariushu@users.noreply.github.com>
2018-04-09 22:28:00 +0200

    5.6 Improvement of Verification structure

--- addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/GroupDeletionTest.java
@@ -1,11 +1,13 @@
 package pl.stqa.ptf.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pl.stqa.ptf.addressbook.model.GroupData;
+import pl.stqa.ptf.addressbook.model.Groups;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTest extends TestBase {
 
@@ -19,16 +21,16 @@ public class GroupDeletionTest extends TestBase {
 
   @Test
   public void testGroupDeletion() {
-    Set<GroupData> before = app.group().all();
+    Groups before = app.group().all();
     GroupData deletedGroup = before.iterator().next();
 
     app.group().delete(deletedGroup);
 
-    Set<GroupData> after = app.group().all();
-    Assert.assertEquals(after.size(),before.size() -1);
+    Groups after = app.group().all();
 
-    before.remove(deletedGroup);
-    Assert.assertEquals(before, after);
+    assertEquals(after.size(),before.size() -1);
+    assertThat(after, equalTo(before.without(deletedGroup)));
+
     }
 
 
