06a14c9ce1145c3d591ee18dbdf9a45c08aa635e
mariushu <36562086+mariushu@users.noreply.github.com>
2018-04-24 22:13:42 +0200

    Lesson nr 5.11 String merging

--- addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/ContactPhoneTests.java
@@ -4,6 +4,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pl.stqa.ptf.addressbook.model.ContactData;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -26,11 +29,21 @@ public class ContactPhoneTests extends TestBase {
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
 
-    assertThat(contact.getHomePhone(), equalTo(cleaned(contactInfoFromEditForm.getHomePhone())));
-    assertThat(contact.getMobilePhone(), equalTo(cleaned(contactInfoFromEditForm.getMobilePhone())));
-    assertThat(contact.getWorkPhone(), equalTo(cleaned(contactInfoFromEditForm.getWorkPhone())));
-    
+    assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+
+
   }
+
+  private String mergePhones(ContactData contact) {
+    return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
+            .stream().filter((s) -> !s.equals(""))
+            .map(ContactPhoneTests::cleaned)
+            .collect(Collectors.joining("\n"));
+
+
+  }
+
+
   public static String cleaned(String phone) {
     return phone.replaceAll("\\s", "").replaceAll("[-()]","");
 
