67857c54417fb3b867fc882bdb2707e28aab8d03
mariushu <36562086+mariushu@users.noreply.github.com>
2018-05-02 21:00:23 +0200

    Homework nr 15
    Sparametryzuj testy dla tworzenia grup i kontaktów.
    Zrealizuj dla testów tworzenia grup i kontaktów providery testowych danych, które wczytują dane
    z plików w jednym z formatów: XML lub JSON.

--- addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/ContactCreationTest.java
@@ -1,24 +1,45 @@
 package pl.stqa.ptf.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.stqa.ptf.addressbook.model.ContactData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ContactCreationTest extends TestBase{
 
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
 
 
-
-
-    @Test
-    public void testContactCreation() {
+    @Test (dataProvider = "validContactsFromXml")
+    public void testContactCreation (ContactData contacts) {
         File photo = new File("src/test/resources/stru.png");
         //Contacts before = app.contact().all();
 
 
-        ContactData contact = new ContactData()
-                .withFirstname("Martin")
+       ContactData contact = new ContactData().withFirstname("").withLastname("").withPhoto(photo)
+               .withAddress("").withTel("").withMail("").withGroup("");    //dane z pliku xml
+                /*.withFirstname("Martin")
                 .withLastname("Grey")
                 .withTel("555 555 555")
                 .withHomePhone("+52 00 000 00")
@@ -27,10 +48,10 @@ public class ContactCreationTest extends TestBase{
                 .withAddress("Polna")
                 .withMail("emial@gmail.com")
                 .withPhoto(photo)
-                .withGroup("[none]");
+                .withGroup("[none]");*/
 
-        app.contact().create(contact);
-        /*assertThat(app.contact().count(), equalTo(before.size() + 1));
+        /*app.contact().create(contacts);
+        assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
 
         assertThat(after, equalTo(
