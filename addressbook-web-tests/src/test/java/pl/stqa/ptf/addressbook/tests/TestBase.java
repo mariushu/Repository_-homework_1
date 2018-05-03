c5b5edc3ad67c477287c70fd96048a68af90f7cf
mariushu <36562086+mariushu@users.noreply.github.com>
2018-05-03 16:23:06 +0200

    Lesson 6.10 Test stand configuration
    Opis konfiguracji testowej podstawy

--- addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/stqa/ptf/addressbook/tests/TestBase.java
@@ -1,13 +1,14 @@
 package pl.stqa.ptf.addressbook.tests;
 
-import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import pl.stqa.ptf.addressbook.appmenager.ApplicationMenager;
+import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-  protected static final ApplicationMenager app = new ApplicationMenager(BrowserType.CHROME);
+  protected static final ApplicationMenager app
+          = new ApplicationMenager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
