b33f1b3b8b8d0b43565877e3f4a2c1f8db3b2949
mariushu <36562086+mariushu@users.noreply.github.com>
2018-06-06 10:31:52 +0200

    Lesson 9.2 SOAP simple examples
    9.2. SOAP: prosty przykład

---
+++ soap-sample/src/test/java/pl/stqa/ptf/soap/GeoIpServiceTests.java
@@ -0,0 +1,17 @@
+package pl.stqa.ptf.soap;
+
+import com.buzzbuzhome.BBHLocation;
+import com.buzzbuzhome.GeoIP;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp() {
+    BBHLocation userLocation = new GeoIP().getGeoIPSoap12().getUserLocation("31.182.251.230");
+    assertEquals(userLocation.getCountryCode(), "PL");
+  }
+
+}
